2a7ff4bcba635106d7580914977cf101318b5151
Simon Lukas ~ Znox / Plänkler <60503970+Plaenkler@users.noreply.github.com>
2022-11-24 00:16:24 +0100

    [UPD] Check http methods
    - Rework section name in logs
    - Handle 404 -> redirect to homepage

--- pkg/router/routes/home.go
+++ pkg/router/routes/home.go
@@ -15,25 +15,34 @@ var (
 	static embed.FS
 )
 
-type homePage struct {
-	Title string
-}
-
 func ProvideHomePage(writer http.ResponseWriter, request *http.Request) {
 	defer handler.HandlePanic("routes")
 
-	log.Printf("[providehomepage] request on /")
+	log.Printf("[provide homepage] request on %s - by address: %s", request.RequestURI, request.RemoteAddr)
+
+	if request.URL.Path != "/" {
+		http.Redirect(writer, request, "/", http.StatusSeeOther)
+		return
+	}
+
+	if request.Method != http.MethodGet {
+		writer.WriteHeader(http.StatusForbidden)
+		_, err := writer.Write([]byte("405 - Method not allowed"))
+		if err != nil {
+			log.Printf("[provide homepage] could not write http reply - error: %s", err)
+		}
+		return
+	}
 
 	template, err := template.New("home").ParseFS(static, "static/html/home.html")
 	if err != nil {
-		fmt.Fprintf(writer, "[providehomepage] could not provide template - error: %s", err)
+		fmt.Fprintf(writer, "[provide homepage] could not provide template - error: %s", err)
+		return
 	}
 
 	writer.Header().Add("Content-Type", "text/html")
-	err = template.Execute(writer, homePage{
-		Title: "Homepage",
-	})
+	err = template.Execute(writer, nil)
 	if err != nil {
-		log.Panicf("[providehomepage] could not execute parsed template - error: %s", err)
+		log.Panicf("[provide homepage] could not execute parsed template - error: %s", err)
 	}
 }
