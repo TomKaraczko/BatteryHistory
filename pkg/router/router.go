18308d5fb149958e961ab59b6938ceba4fa3bd52
Simon Lukas ~ Znox / Plänkler <60503970+Plaenkler@users.noreply.github.com>
2023-01-10 21:01:42 +0100

    [REM] Orphan folder

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -72,7 +72,6 @@ func (manager *Manager) provideFiles() error {
 
 	manager.Router.Handle("/css/", http.FileServer(http.FS(fs)))
 	manager.Router.Handle("/img/", http.FileServer(http.FS(fs)))
-	manager.Router.Handle("/js/", http.FileServer(http.FS(fs)))
 
 	return nil
 }
